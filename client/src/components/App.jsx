6035af85247f2da086f47e96d8655c662d5e6055
Roswell Cua <ross.cua@gmail.com>
2021-06-15 14:34:28 -0700

    test 4

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import IntroPage from './IntroPage.jsx';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import AboutMe from './AboutMe.jsx';
 import Projects from './Projects.jsx';
 import NavBar from './NavBar.jsx';
@@ -12,7 +12,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router>
+      <Router basename="/rcuaPortfolio">
         <NavBar />
         <Route
           exact
